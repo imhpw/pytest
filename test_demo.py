486a1a3ef7dc58477f1d21a70c46144dc41d18ed
quailty <imhuangpw@gmail.com>
2021-01-20 09:59:45 +0800

    second commit

--- test_demo.py
+++ test_demo.py
@@ -1,3 +1,19 @@
 import pytest
-from testing import *
-demo2.Demo2()
+from python.calc import Calc
+
+
+class TestCalc:
+
+    def test_add_1(self):
+        self.calc = Calc()
+        result = self.calc.add(1,3)
+        print(result)
+        assert 4 == result
+
+    def test_div(self):
+        self.calc = Calc()
+        result = self.calc.div(2,2)
+        assert 1 == result
+
+if __name__ == '__main__':
+    pytest.main(['-v', 'test_demo.py::TestCalc'])
